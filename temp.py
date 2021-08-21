d7c51815c72cdb2f3f504a0a8096c5d8b671baf2
Wesley Lum <wesley.lum2020@gmail.com>
2021-08-21 16:08:32 +0100

    first repo

---
+++ temp.py
@@ -0,0 +1,20 @@
+flat = 1
+house = 2
+total = 0
+
+# for i in range(1000):
+#     total = total+flat+house
+
+# print(total)
+
+
+
+Yearly_Interest = 10
+Yearly_Charge = 1
+total = 1000
+
+for i in range(100):
+    total = total+Yearly_Interest-Yearly_Charge
+
+#print(total)
+          
